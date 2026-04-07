14dee66fe01c5fd1bef950895f03fd1cf421da32
SuperSayianZilla <48083027+SuperSayianZilla@users.noreply.github.com>
2026-04-06 20:09:30 -0400

    Add files via upload

---
+++ modinfo.lua
@@ -0,0 +1,13 @@
+return {
+	name = 'Siegebots 2.81',
+	description = 'New Fac!',
+	shortname = 'SiegeBot 2.81',
+	version = '2.81',
+	mutator = '1',
+	game = 'Siegebots',
+	shortGame = 'SiegeBots',
+	modtype = 1,
+	depend = {
+		[[rapid://zk:stable]]
+	},
+}
